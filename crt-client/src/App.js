9b907b561a95d4265ae7edf672eb991a9a337188
MPRoses <jensvanderslootgaming@gmail.com>
2023-01-16 20:20:13 +0100

    first commit

---
+++ crt-client/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     <form action="APPEL" method="post" 
+              className="form">
+          <button type="submit">Connected?</button>
+        </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.post("/APPEL", (req, res) => {
+  console.log("Example post function");
+  res.redirect("/");
+});
+
+const port = process.env.port || 3001;
+
+app.listen(port, console.log(`Backend has been activated on port ${port} and frontEnd on port 3000`))
