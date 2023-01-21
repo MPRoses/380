2bf05ecc23bdb1c82bed8fda84306cc462b1d94a
MPRoses <jensvanderslootgaming@gmail.com>
2023-01-21 17:23:32 +0100

    bug_fix

--- crt-client/src/home/home.jsx
+++ crt-client/src/home/home.jsx
@@ -93,6 +93,8 @@ const Home = ({ children }) => {
 
       <Cursor />
       <Preloader />
+
+      
           
       <div id="Hero">
       <div id="left-container">
@@ -127,6 +129,15 @@ const Home = ({ children }) => {
 
           </div>
         </div>
+        <div className="circular-text fadein">
+            <p className="text">Scroll • Verder • Voor • Meer • Info •</p>
+            <div className="arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512.568 512.568" style={{"enable-background": "new 0 0 512.568 512.568"}} xmlSpace="preserve"  width="512" height="512">
+                    <path d="M184.284,235.284h140v40h-140V235.284z M275.284,436V315.284h-40V436l-53.858-53.858l-28.284,28.285l102.142,102.142  l105.142-105.142l-28.284-28.285L275.284,436z M235.284,76.568v118.716h40V76.568l56.858,56.858l28.284-28.284L255.284,0  L152.142,103.142l28.284,28.284L235.284,76.568z"/>
+                </svg>
+            </div>
+
+        </div>
         <div id="get-better-grades" className="fadein">
             HAAL<br></br>BETERE<br></br>RESULTATEN
           </div>
@@ -137,15 +148,7 @@ const Home = ({ children }) => {
           </div>
 
           <div className="container fadein">  
-        <div className="circular-text">
-            <p className="text">Scroll • Verder • Voor • Meer • Info •</p>
-            <div className="arrow">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512.568 512.568" style={{"enable-background": "new 0 0 512.568 512.568"}} xmlSpace="preserve"  width="512" height="512">
-                    <path d="M184.284,235.284h140v40h-140V235.284z M275.284,436V315.284h-40V436l-53.858-53.858l-28.284,28.285l102.142,102.142  l105.142-105.142l-28.284-28.285L275.284,436z M235.284,76.568v118.716h40V76.568l56.858,56.858l28.284-28.284L255.284,0  L152.142,103.142l28.284,28.284L235.284,76.568z"/>
-                </svg>
-            </div>
-
-        </div>
+        
     </div>
         </div>
       <div id="Fixed">
@@ -190,7 +193,7 @@ const Home = ({ children }) => {
           <div id="left-background"></div>
         </div>
       <div id="S2">
-        
+
       </div>
   
       </div>
